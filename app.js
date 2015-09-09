640f303822a9de47e2c8657cd65cedcb280bc185
junedev <catherinejune86@gmail.com>
2015-09-09 13:05:19 +0100

    app working and styled

--- app.js
+++ app.js
@@ -2,26 +2,51 @@ var morgan  = require('morgan');
 var express = require('express');
 var bodyParser = require("body-parser");
 var ejs = require("ejs");
+var sassMiddleware = require('node-sass-middleware');
 var app     = express();
 var port    = process.env.PORT || 3000;
 var router  = express.Router();
 var server  = require('http').createServer(app);
-var stream = null;
+var stream1 = null;
+var stream2 = null;
+var currentSearch = null;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.set('views', './views');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static(__dirname + '/public'));
 
 app.use(morgan('dev'));
 
+app.use(sassMiddleware({
+  src: __dirname + '/scss', 
+  dest: __dirname + '/public/css',
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: "/css"
+  }),
+  express.static(__dirname + '/public')
+)
+
 router.get('/', function(req, res) {
   io.on('connect', function(socket) {
-    socket.on("search", function(searchTerm){
-      if(searchTerm){
-        stream = twitter.stream('statuses/filter', { track: searchTerm });
-        stream.on('tweet', function(tweet) {
+    socket.on("stop", function(){
+      console.log("stop received");
+        stream1.stop();
+        stream2.stop();
+        stream1 = null;
+        stream2 = null;
+        currentSearch = null;
+        socket.disconnect(true);
+    })
+
+    socket.on("search", function(searchTerms){
+      if(!currentSearch && searchTerms){
+        currentSearch = searchTerms;
+        console.log(searchTerms);
+        stream1 = twitter.stream('statuses/filter', { track: searchTerms.search1 });
+        stream2 = twitter.stream('statuses/filter', { track: searchTerms.search2 });
+        stream1.on('tweet', function(tweet) {
           var data = {};
           data.name = tweet.user.name;
           data.screen_name = tweet.user.screen_name;
@@ -29,16 +54,28 @@ router.get('/', function(req, res) {
           data.profile_image_url = tweet.user.profile_image_url;
           socket.emit('tweet1', data);
         });
+        stream2.on('tweet', function(tweet) {
+          var data = {};
+          data.name = tweet.user.name;
+          data.screen_name = tweet.user.screen_name;
+          data.text = tweet.text;
+          data.profile_image_url = tweet.user.profile_image_url;
+          socket.emit('tweet2', data);
+        });
       }
     });
-
-    socket.on("stop", function(){
-      if(stream) { stream.stop() };
-    })
 
   });
   res.render('index.html');
 });
+
+// router.post("/stop", function(req,res){
+//   stream1.stop();
+//   stream2.stop();
+//   stream1 = null;
+//   stream2 = null;
+//   res.send(200);
+// })
 
 app.use('/', router);
 server.listen(port);
--- public/js/client_app.js
+++ public/js/client_app.js
@@ -1,6 +1,8 @@
-angular.module("twitterRace",[])
+angular.module("twitterRace",["firebase"])
 .factory("socket", socket)
-.controller("MainController", MainController);
+.factory("Score",Score)
+.controller("MainController", MainController)
+.constant("FIREBASE_URL","https://twitter-race.firebaseio.com/");
 
 socket.$inject = ["$rootScope"]
 
@@ -28,29 +30,124 @@ function socket($rootScope){
   };
 }
 
+Score.$inject = ["$firebaseArray","FIREBASE_URL"];
 
-MainController.$inject = ["socket"]
+function Score($firebaseArray,FIREBASE_URL){
+  var ref = new Firebase(FIREBASE_URL);
+  var scores = $firebaseArray(ref.child("scores"));
 
-function MainController(socket){
+  var Score = {};
+
+  Score.all = scores;
+
+  Score.create = function(score){
+    return scores.$add(score);
+  }
+
+  Score.delete = function(score){
+    return scores.$remove(score);
+  }
+
+  return Score;
+}
+
+
+
+MainController.$inject = ["socket","$http","$window","Score"]
+
+function MainController(socket,$http,$window,Score){
   var self = this;
-  self.searchTerm = "";
+  var intervalID = null;
+  self.searchTerms = {};
+  self.searchTermsCopy = {};
   self.tweets1 = [];
+  self.tweets2 = [];
+  self.status = 0;
+  self.counter1 = 0;
+  self.counter2 = 0;
+  self.bar = {};
+  self.countdown = 15;
+  self.scores = Score.all;
 
-  socket.on('connect', function() {
-    console.log("Connected");
+  self.calc = function(){
+    self.bar = {left: self.counter1*10, right: self.counter2*10};
+    if(self.counter1 > 30 || self.counter2 > 30){
+      if(self.counter1>self.counter2){
+        self.bar = {
+          left: 300,
+          right: 300*self.counter2/self.counter1
+        }
+      } else {
+        self.bar = {
+          left: 300*self.counter1/self.counter2,
+          right: 300
+        }
+      }
+    }
+  }
+
+  socket.on('tweet2', function(tweet) {
+    if(self.status < 2){
+      self.status++;
+      if(self.status === 2){
+        self.status++;
+        self.startCountdown();
+        $window.setTimeout(function() {
+          socket.emit('stop');
+          self.status = 5;
+          $window.clearInterval(intervalID);
+        Score.create({
+          1:[self.searchTerms.search1,self.counter1],
+          2:[self.searchTerms.search2,self.counter2]
+        })
+      }, 15000);
+      }
+    } else {
+      self.counter2++;
+      self.tweets2.unshift(tweet);
+      if (self.tweets2.length > 10) self.tweets2.pop();
+    }
+    self.calc();
   });
 
   socket.on('tweet1', function(tweet) {
-    self.tweets1.unshift(tweet);
-    if (self.tweets1.length > 10) self.tweets1.pop();
+    if(self.status < 2){
+      self.status++;
+      if(self.status === 2){
+        self.status++;
+        self.startCountdown();
+        $window.setTimeout(function() {
+          socket.emit('stop');
+          self.status = 5;
+          $window.clearInterval(intervalID);
+          Score.create({
+            1:[self.searchTerms.search1,self.counter1],
+            2:[self.searchTerms.search2,self.counter2]
+          })
+      }, 15000);
+      }
+    } else {
+      self.counter1++;
+      self.tweets1.unshift(tweet);
+      if (self.tweets1.length > 10) self.tweets1.pop();
+    }
+    self.calc();
   });
 
+
   self.sendSearchTerm = function(){
-    console.log("function reached");
-    socket.emit('search', self.searchTerm);
+    socket.emit('search', self.searchTerms);
+    self.searchTermsCopy = self.searchTerms;
   }
 
-  self.stopStream = function(){
-    socket.emit('stop');
+  self.restart = function(){
+    $window.location.href = "/";
   }
+
+  self.startCountdown = function(){
+    intervalID = $window.setInterval(function() {
+      self.countdown--;
+    }, 1000);
+  }
+
 }
