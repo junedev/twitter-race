be1d118ded2e9b5208755a707c1b1ecba01c2307
junedev <catherinejune86@gmail.com>
2015-09-09 15:55:02 +0100

    listen command moved to end of app.js to fix heroku version

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var ejs = require("ejs");
 var sassMiddleware = require('node-sass-middleware');
 var app     = express();
-var port    = process.env.PORT || 3000;
+var port    = process.env.PORT || 9000;
 var router  = express.Router();
 var server  = require('http').createServer(app);
 var stream1 = null;
@@ -27,6 +27,7 @@ app.use(sassMiddleware({
   }),
   express.static(__dirname + '/public')
 )
+
 
 router.get('/', function(req, res) {
   io.on('connect', function(socket) {
@@ -69,19 +70,10 @@ router.get('/', function(req, res) {
   res.render('index.html');
 });
 
-// router.post("/stop", function(req,res){
-//   stream1.stop();
-//   stream2.stop();
-//   stream1 = null;
-//   stream2 = null;
-//   res.send(200);
-// })
 
 app.use('/', router);
-server.listen(port);
 
-console.log('Server started on ' + port);
-
+//var io = require('socket.io').listen(server);
 var io   = require('socket.io')(server);
 var Twit = require('twit');
 var twitter = new Twit({
@@ -91,3 +83,4 @@ var twitter = new Twit({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET2
 });
 
+server.listen(port);
